e9128ceaa18ac649dbe504ad8d535835406568eb
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-04-26 19:05:29 -0300

    test: ensure EmailValidatorAdapter returns true if validator returns true

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,9 @@
+import { EmailValidatorAdapter } from './email-validator-adapter'
+
+describe('EmailValidator', () => {
+  test('should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid_email@mail.com')
+    expect(isValid).toBe(true)
+  })
+})
