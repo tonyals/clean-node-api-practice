059723375a471ea322be6460054151608f7e504f
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-04-27 22:33:30 -0300

    feat: ensure requests return json as default content-type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { cors } from '../middlewares/cors'
+import { contentType } from '../middlewares/content-type'
 
 export default (app: Express): void => {
   app.use(cors)
+  app.use(contentType)
 }
