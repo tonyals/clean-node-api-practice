4189d6ff32465d2be28642ddd89f76abbfddb26f
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-05-05 15:48:13 -0300

    feat: ensure LoginController returns 400 if no email is provided

---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,14 @@
+import { Controller, HttpResponse, HttpRequest } from '../../protocols'
+import { badRequest } from '../../helpers/http-helper'
+import { MissingParamError } from '../../errors'
+
+export class LoginController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const requiredFields = ['email']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return new Promise(resolve => resolve(badRequest(new MissingParamError(field))))
+      }
+    }
+  }
+}
